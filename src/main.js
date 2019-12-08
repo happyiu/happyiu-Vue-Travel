09ebbf87ae273103b7b3434aece951c5977c7fde
happyiu <910785329@qq.com>
2019-12-08 22:20:23 +0800

    swiper-one

--- src/main.js
+++ src/main.js
@@ -6,9 +6,13 @@ import fsatClick from 'fastclick'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/styles/iconFont/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fsatClick.attach(document.body)
+
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   router,
---
+++ src/views/home/components/swiper.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper">
+    <!-- slides -->
+    <swiper-slide v-for="item in swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.url">
+    </swiper-slide>
+
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'HomeWwiper',
+    data(){
+      return {
+        swiperOption: {
+          pagination: {
+            el: '.swiper-pagination'
+          },
+          loop: true
+        },
+        swiperList: [
+          {
+            id: '01',
+            url: "//imgs.qunarzz.com/vc/aa/9e/82/14bf523635542806a671763e98.jpg_92.jpg"
+          },
+          {
+            id: '02',
+            url: "//imgs.qunarzz.com/vc/44/e9/86/95bc36c9e1c06ebd68bdfe222e.jpg_92.jpg"
+          }
+        ]
+      }
+    },
+    computed: {
+      swiper() {
+        return this.$refs.mySwiper.swiper
+      }
+    },
+  }
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active 
+  background white
+
+.wrapper
+  width 100%
+  height 0
+  overflow hidden
+  padding-bottom 31.25%
+  background #eee
+  .swiper-img
+    width 100%
+</style>
