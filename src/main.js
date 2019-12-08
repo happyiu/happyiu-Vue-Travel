c5a5d331dbe83df38f016823279f1967dae45f91
happyiu <910785329@qq.com>
2019-12-08 21:31:08 +0800

    one

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import fsatClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import './assets/styles/iconFont/iconfont.css'
 
 Vue.config.productionTip = false
+fsatClick.attach(document.body)
 
 new Vue({
   router,
---
+++ src/views/home/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont backIcon">&#xe618;</div>
+    </div>
+    <div class="header-input"><span class="iconfont">&#xe627;</span>输入城市/景点/游玩主题</div>
+    <div class="header-right">
+      城市<span class="iconfont">&#xe695;</span>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~@/assets/styles/varibles.styl";
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: white
+    .header-left
+      width: .64rem
+      float: left 
+      .backIcon 
+        text-align: center 
+    .header-input
+      flex: 1
+      background: white
+      margin-top: .12rem
+      height: .64rem
+      line-height: .64rem
+      border-radius: .1rem
+      color: #ccc
+      padding-left: .20rem
+    .header-right
+      width: 1.24rem
+      float right
+      text-align: center
+    
+</style>
